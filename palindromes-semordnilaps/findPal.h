5338d6615f5d8b0042803168c89021c9f529ff41
Cadde0 <ocaddeo@kth.se>
2025-01-27 09:23:56 +0100

    is_pal method start
    pseudo code for sequential part of is_pal

--- palindromes-semordnilaps/findPal.h
+++
@@ -1,6 +0,0 @@
-#ifndef FINDPAL_H
-#define FINDPAL_H
-
-int is_pal();
-
-#endif // FINDPAL_H
--- palindromes-semordnilaps/main.c
+++ palindromes-semordnilaps/main.c
@@ -8,7 +8,7 @@
 
 #include "findPal.h"
 
-#define MAX_WORDS 5
+#define MAX_WORDS 235977
 
 int store_words(const char *filename, char **words)
 {
@@ -25,7 +25,7 @@ int store_words(const char *filename, char **words)
     {
         // Remove newline character
         line[strcspn(line, "\n")] = '\0';
-
+        // Word file ends with a newline
         if (strlen(line) == 0)
         {
             break;
@@ -39,11 +39,37 @@ int store_words(const char *filename, char **words)
     return word_count;
 }
 
+int is_pal(char *word, char **words)
+{
+    int len = strlen(word);
+    char *reversed = malloc(len * sizeof(char));
+    for (int i = 0; i < len; i++)
+    {
+        *(reversed + i) = *(word + len - i - 1);
+    }
+
+    while (/*Condition*/)
+    {
+        if (/*reversed word is in dict*/)
+        {
+            /* write word to result file */
+            return 1; // Is a pal
+        }
+    }
+
+    free(reversed);
+
+    return 0;
+}
+
 int main(int argc, char const *argv[])
 {
+
     const char *input_file = argv[1];
+    // Allocate memory for the words
     char **words = malloc(MAX_WORDS * sizeof(char *));
 
+    // Store the words in the array
     int word_count = store_words(input_file, words);
 
     // Print the words to verify
