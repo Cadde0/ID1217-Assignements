36bd6c4e85c13352b99aba1ec7b5169ee7c6c6ad
Cadde0 <ocaddeo@kth.se>
2025-02-16 18:19:06 +0100

    Added text for H3

--- unisex-bathroom/main.c
+++ unisex-bathroom/main.c
@@ -3,96 +3,117 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include <unistd.h>
+#include <fcntl.h> // For O_CREAT and O_EXCL
 
-#define NUM_PEOPLE 1000
+#define NUM_PEOPLE 10
 
-sem_t bathroom;
-sem_t queue_lock;
+sem_t *bathroom;
+sem_t *queue_lock;
 int men_in_bathroom = 0, women_in_bathroom = 0;
 int waiting_men = 0, waiting_women = 0;
 
-void *man(void *arg) {
+void *man(void *arg)
+{
     int id = *(int *)arg;
-    
-    while (1) {
-        sleep(rand() % 5 + 1);
-        
-        sem_wait(&queue_lock);
+
+    while (1)
+    {
+        sleep(rand() % 5 + 3);
+
+        sem_wait(queue_lock);
         waiting_men++;
-        while (women_in_bathroom > 0) {
-            sem_post(&queue_lock);
-            sem_wait(&bathroom);
-            sem_wait(&queue_lock);
+        printf("Man %d is waiting. Men in queue: %d, Women in queue: %d\n", id, waiting_men, waiting_women);
+        while (women_in_bathroom > 0)
+        {
+            sem_post(queue_lock);
+            sem_wait(bathroom);
+            sem_wait(queue_lock);
         }
         waiting_men--;
         men_in_bathroom++;
-        printf("Man %d entered the bathroom. Men inside: %d\n", id, men_in_bathroom);
-        sem_post(&queue_lock);
-        
+        printf("Man %d entered the bathroom. Men inside: %d, Women inside: %d\n", id, men_in_bathroom, women_in_bathroom);
+        sem_post(queue_lock);
+
         sleep(rand() % 3 + 1);
-        
-        sem_wait(&queue_lock);
+
+        sem_wait(queue_lock);
         men_in_bathroom--;
-        printf("Man %d left the bathroom. Men inside: %d\n", id, men_in_bathroom);
-        if (men_in_bathroom == 0 && waiting_women > 0) {
-            for (int i = 0; i < waiting_women; i++) sem_post(&bathroom);
+        printf("Man %d left the bathroom. Men inside: %d, Women inside: %d\n", id, men_in_bathroom, women_in_bathroom);
+        if (men_in_bathroom == 0 && waiting_women > 0)
+        {
+            for (int i = 0; i < waiting_women; i++)
+                sem_post(bathroom);
         }
-        sem_post(&queue_lock);
+        sem_post(queue_lock);
     }
     return NULL;
 }
 
-void *woman(void *arg) {
+void *woman(void *arg)
+{
     int id = *(int *)arg;
-    
-    while (1) {
-        sleep(rand() % 5 + 1);
-        
-        sem_wait(&queue_lock);
+
+    while (1)
+    {
+        sleep(rand() % 5 + 3);
+
+        sem_wait(queue_lock);
         waiting_women++;
-        while (men_in_bathroom > 0) {
-            sem_post(&queue_lock);
-            sem_wait(&bathroom);
-            sem_wait(&queue_lock);
+        printf("Woman %d is waiting. Women in queue: %d, Men in queue: %d\n", id, waiting_women, waiting_men);
+        while (men_in_bathroom > 0)
+        {
+            sem_post(queue_lock);
+            sem_wait(bathroom);
+            sem_wait(queue_lock);
         }
         waiting_women--;
         women_in_bathroom++;
-        printf("Woman %d entered the bathroom. Women inside: %d\n", id, women_in_bathroom);
-        sem_post(&queue_lock);
-        
+        printf("Woman %d entered the bathroom. Women inside: %d, Men inside: %d\n", id, women_in_bathroom, men_in_bathroom);
+        sem_post(queue_lock);
+
         sleep(rand() % 3 + 1);
-        
-        sem_wait(&queue_lock);
+
+        sem_wait(queue_lock);
         women_in_bathroom--;
-        printf("Woman %d left the bathroom. Women inside: %d\n", id, women_in_bathroom);
-        if (women_in_bathroom == 0 && waiting_men > 0) {
-            for (int i = 0; i < waiting_men; i++) sem_post(&bathroom);
+        printf("Woman %d left the bathroom. Women inside: %d, Men inside: %d\n", id, women_in_bathroom, men_in_bathroom);
+        if (women_in_bathroom == 0 && waiting_men > 0)
+        {
+            for (int i = 0; i < waiting_men; i++)
+                sem_post(bathroom);
         }
-        sem_post(&queue_lock);
+        sem_post(queue_lock);
     }
     return NULL;
 }
 
-int main() {
+int main()
+{
     pthread_t people[NUM_PEOPLE];
-    sem_init(&bathroom, 0, 0);
-    sem_init(&queue_lock, 0, 1);
-    
-    for (int i = 0; i < NUM_PEOPLE; i++) {
+    bathroom = sem_open("/bathroom_sem", O_CREAT, 0644, 0);
+    queue_lock = sem_open("/queue_lock_sem", O_CREAT, 0644, 1);
+
+    for (int i = 0; i < NUM_PEOPLE; i++)
+    {
         int *id = malloc(sizeof(int));
         *id = i;
-        if (rand() % 2 == 0) {
+        if (rand() % 2 == 0)
+        {
             pthread_create(&people[i], NULL, man, id);
-        } else {
+        }
+        else
+        {
             pthread_create(&people[i], NULL, woman, id);
         }
     }
-    
-    for (int i = 0; i < NUM_PEOPLE; i++) {
+
+    for (int i = 0; i < NUM_PEOPLE; i++)
+    {
         pthread_join(people[i], NULL);
     }
-    
-    sem_destroy(&bathroom);
-    sem_destroy(&queue_lock);
+
+    sem_close(bathroom);
+    sem_close(queue_lock);
+    sem_unlink("/bathroom_sem");
+    sem_unlink("/queue_lock_sem");
     return 0;
 }
--- unisex-bathroom/solution.c
+++ unisex-bathroom/solution.c
@@ -4,12 +4,13 @@
 #include <semaphore.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <fcntl.h> // For O_CREAT and O_EXCL
 
 #define NUM_PEOPLE 1000
 
-sem_t bathroom_lock;
-sem_t queue_lock;      
-int count = 0; 
+sem_t *bathroom_lock;
+sem_t *queue_lock;
+int count = 0;
 char current_gender = 'N';
 
 typedef struct
@@ -18,7 +19,8 @@ typedef struct
     char gender;
 } Person;
 
-typedef struct {
+typedef struct
+{
     Person *people[NUM_PEOPLE];
     int front;
     int rear;
@@ -27,22 +29,27 @@ typedef struct {
 
 Queue queue;
 
-void init_queue() {
+void init_queue()
+{
     queue.front = 0;
     queue.rear = -1;
     queue.size = 0;
 }
 
-void enqueue(Person *person) {
-    if (queue.size < NUM_PEOPLE) {
+void enqueue(Person *person)
+{
+    if (queue.size < NUM_PEOPLE)
+    {
         queue.rear = (queue.rear + 1) % NUM_PEOPLE;
         queue.people[queue.rear] = person;
         queue.size++;
     }
 }
 
-Person* dequeue() {
-    if (queue.size > 0) {
+Person *dequeue()
+{
+    if (queue.size > 0)
+    {
         Person *person = queue.people[queue.front];
         queue.front = (queue.front + 1) % NUM_PEOPLE;
         queue.size--;
@@ -51,77 +58,94 @@ Person* dequeue() {
     return NULL;
 }
 
-bool is_queue_empty() {
+bool is_queue_empty()
+{
     return queue.size == 0;
 }
 
-void *use_bathroom(void *arg) {
+void *use_bathroom(void *arg)
+{
     Person *person = (Person *)arg;
-    while (1) {
+    while (1)
+    {
         sleep(rand() % 3 + 1);
 
-        sem_wait(&queue_lock);
+        sem_wait(queue_lock);
         enqueue(person);
-        sem_post(&queue_lock);
+        sem_post(queue_lock);
 
-        while (1) {
-            sem_wait(&queue_lock);
+        while (1)
+        {
+            sem_wait(queue_lock);
             Person *next_person = queue.people[queue.front];
-            if (next_person == person) {
-                sem_wait(&bathroom_lock);
-                if (count == 0) {
+            if (next_person == person)
+            {
+                sem_wait(bathroom_lock);
+                if (count == 0)
+                {
                     current_gender = person->gender;
                 }
-                if (current_gender == person->gender) {
+                if (current_gender == person->gender)
+                {
                     count++;
                     dequeue();
                     printf("%c %d entered the bathroom. Count: %d\n", person->gender, person->id, count);
-                    sem_post(&bathroom_lock);
-                    sem_post(&queue_lock);
+                    sem_post(bathroom_lock);
+                    sem_post(queue_lock);
                     break;
-                } else {
-                    sem_post(&bathroom_lock);
-                    sem_post(&queue_lock);
                 }
-            } else {
-                sem_post(&queue_lock);
+                else
+                {
+                    sem_post(bathroom_lock);
+                    sem_post(queue_lock);
+                }
+            }
+            else
+            {
+                sem_post(queue_lock);
             }
         }
 
         sleep(rand() % 2 + 1);
 
-        sem_wait(&bathroom_lock);
+        sem_wait(bathroom_lock);
         count--;
         printf("%c %d left the bathroom. Count: %d\n", person->gender, person->id, count);
-        if (count == 0) {
+        if (count == 0)
+        {
             current_gender = (current_gender == 'M') ? 'F' : 'M';
         }
-        sem_post(&bathroom_lock);
+        sem_post(bathroom_lock);
     }
     return NULL;
 }
 
-int main() {
+int main()
+{
     pthread_t people[NUM_PEOPLE];
     Person persons[NUM_PEOPLE];
-    
-    sem_init(&bathroom_lock, 0, NUM_PEOPLE);
-    sem_init(&queue_lock, 0, 1); 
-    
+
+    bathroom_lock = sem_open("/bathroom_lock_sem", O_CREAT, 0644, NUM_PEOPLE);
+    queue_lock = sem_open("/queue_lock_sem", O_CREAT, 0644, 1);
+
     init_queue();
-    
-    for (int i = 0; i < NUM_PEOPLE; i++) {
+
+    for (int i = 0; i < NUM_PEOPLE; i++)
+    {
         persons[i].id = i;
         persons[i].gender = (i % 2) ? 'M' : 'F';
         pthread_create(&people[i], NULL, use_bathroom, &persons[i]);
     }
-    
-    for (int i = 0; i < NUM_PEOPLE; i++) {
+
+    for (int i = 0; i < NUM_PEOPLE; i++)
+    {
         pthread_join(people[i], NULL);
     }
-    
-    sem_destroy(&bathroom_lock);
-    sem_destroy(&queue_lock);
-    
+
+    sem_close(bathroom_lock);
+    sem_close(queue_lock);
+    sem_unlink("/bathroom_lock_sem");
+    sem_unlink("/queue_lock_sem");
+
     return 0;
 }
